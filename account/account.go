df2a48968b1923b2b9e39ac316afd517bc101f4b
yasasvi <yasasvigumma@gmail.com>
2024-07-16 22:00:07 +0530

    Expanding code
    Also added a database.

--- account/account.go
+++ account/account.go
@@ -1,23 +1,78 @@
 package account
 
 import (
-  "fmt"
-  _ "database/sql"
+	_ "database/sql"
+	"fmt"
+
+	"math/rand"
 )
 
 type Account struct {
-  AccountNumber int
-  Name string
-  Balance float64
-  password string
+	AccountNumber int64
+	Name          string
+	Balance       float64
+	password      int
 }
 
-func CheckBalance() {
-	fmt.Println("Your balance is: ")
+func insertDB(user Account) {
+	// Insert into database.
+}
+
+func fetchDB(accountNumber int32, password int) {
+	// Fetch from database.
 }
 
 func CreateAccount() {
+	var user Account
+	var verifyPassword int
+	var confirmPassword int
+
+	var err error
+
+	fmt.Println("Enter your name: ")
+	fmt.Scanln(&user.Name)
+
+	fmt.Println("Enter your password: ")
+	_, err = fmt.Scanln(&verifyPassword)
+	fmt.Println("Confirm your password: ")
+	_, err = fmt.Scanln(&confirmPassword)
+
+	if verifyPassword != confirmPassword {
+		fmt.Println("Passwords do not match!")
+		return
+	} else {
+		user.password = verifyPassword
+	}
+
+	fmt.Println("Enter your initial deposit: ")
+	_, err = fmt.Scanln(&user.Balance)
+
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+  
+  // Generate a random account number.
+  user.AccountNumber = rand.Int63n(1000000000000000)
+
+	insertDB(user)
+
 	fmt.Println("Account created successfully!")
+}
+
+func CheckBalance() {
+	var accountNumber int32
+	var password int
+
+	fmt.Println("Enter your account number: ")
+	fmt.Scanln(&accountNumber)
+
+	fmt.Println("Enter your password: ")
+	fmt.Scanln(&password)
+
+	fetchDB(accountNumber, password)
+
+	fmt.Println("Your balance is: ")
 }
 
 func DepositMoney() {
