19a141e86a8eb8f4f8f17fb34dc356c37126759f
yasasvi <yasasvigumma@gmail.com>
2024-08-11 12:07:06 +0530

    Adding .gitignore files
    will help us minimise the repo size.

--- Database/main.go
+++ Database/main.go
@@ -17,17 +17,19 @@ type Data struct {
 	Message string `json:"message"`
 }
 
+func createAccount(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	msg := functions.CreateAccount()
+	data := Data{Message: msg}
+	json.NewEncoder(w).Encode(data)
+}
+
 func main() {
 	fmt.Println("Welcome to Dummy Bank!")
-  PORT := os.Getenv("PORT")
-  if PORT == "" {
-    PORT = "8080"
-  }
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "application/json")
-    msg := functions.CreateAccount()
-		data := Data{Message: msg}
-		json.NewEncoder(w).Encode(data)
-	})
-  http.ListenAndServe(":"+PORT, nil)
+	PORT := os.Getenv("PORT")
+	if PORT == "" {
+		PORT = "8080"
+	}
+	http.HandleFunc("/", createAccount)
+	http.ListenAndServe(":"+PORT, nil)
 }
