382ee0f61f0c096ea1b0900af2aa0167b7174653
yasasvi <yasasvigumma@gmail.com>
2024-11-14 14:03:35 +0530

    updated code
    used different code...

--- cmd/cli.go
+++ cmd/cli.go
@@ -9,68 +9,67 @@ import (
 )
 
 func help() {
-  fmt.Println("Available commands:")
-  fmt.Println("help - show this help")
-  fmt.Println("create - create a new account")
-  fmt.Println("deposit - deposit money")
-  fmt.Println("withdraw - withdraw money")
-  fmt.Println("balance - check balance")
-  fmt.Println("transactions - check transactions")
-  fmt.Println("transfer - transfer money")
-  fmt.Println("exit - exit the application")
+	commands := map[string]string{
+		"help":         "show this help",
+		"create":       "create a new account",
+		"deposit":      "deposit money",
+		"withdraw":     "withdraw money",
+		"balance":      "check balance",
+		"transactions": "check transactions",
+		"transfer":     "transfer money",
+		"exit":         "exit the application",
+	}
+
+	fmt.Println("Available commands:")
+	for command, description := range commands {
+		fmt.Printf("%-12s - %s\n", command, description)
+	}
 }
 
 func get_response(url string) (string, error) {
-  res, err := http.Get("http://localhost:11000/"+url)
-  if err != nil {
-    return "", err
-  }
-  defer res.Body.Close()
-  body, err := io.ReadAll(res.Body)
-  if err != nil {
-    return "", err
-  }
-  var bodyRes database.Response
-  err = json.Unmarshal(body, &bodyRes)
-  if err != nil {
-    return "", err
-  }
-  return bodyRes.Message, nil
+	res, err := http.Get("http://localhost:11000/" + url)
+	if err != nil {
+		return "", err
+	}
+	defer res.Body.Close()
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return "", err
+	}
+	var bodyRes database.Response
+	err = json.Unmarshal(body, &bodyRes)
+	if err != nil {
+		return "", err
+	}
+	return bodyRes.Message, nil
 }
 
 func fetch_responce(url string) {
-  res, err := get_response(url)
-  if err != nil {
-    fmt.Println("Error: ", err)
-  } else {
-    fmt.Println(res)
-  }
+	res, err := get_response(url)
+	if err != nil {
+		fmt.Println("Error: ", err)
+	} else {
+		fmt.Println(res)
+	}
 }
 
 func Menu() {
-  var cmd string
-  for true {
-    fmt.Print("Enter command: ")
-    fmt.Scanln(&cmd)
-    switch cmd {
-    case "help":
-      help()
-    case "create":
-      fetch_responce(cmd)
-    case "deposit":
-      fetch_responce(cmd)
-    case "withdraw":
-      fetch_responce(cmd)
-    case "balance":
-      fetch_responce(cmd)
-    case "transactions":
-      fetch_responce(cmd)
-    case "transfer":
-      fetch_responce(cmd)
-    case "exit":
-      return
-    default:
-      fmt.Println("Invalid command")
-    }
-  }
+	var cmd string
+	for true {
+		fmt.Print("Enter command: ")
+		fmt.Scanln(&cmd)
+		switch cmd {
+		case "help":
+			help()
+		case 
+      "create", "deposit", 
+      "withdraw", "balance", 
+      "transactions", "transfer":
+			fetch_responce(cmd)
+		case "exit":
+			return
+		default:
+			fmt.Println("Invalid command")
+		}
+	}
 }
