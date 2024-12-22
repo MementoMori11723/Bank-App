55c356136440eb3c74592006ef386d4385acfa30
yasasvi <yasasvigumma@gmail.com>
2024-12-22 13:15:42 +0530

    Minimized the code
    ...

--- cli/menu.go
+++ cli/menu.go
@@ -2,23 +2,44 @@ package cli
 
 import "fmt"
 
+type command map[string]string
+
+var (
+	commands = command{
+		"create":       "create a new account",
+		"deposit":      "deposit money",
+		"withdraw":     "withdraw money",
+		"balance":      "check balance",
+		"transactions": "check transactions",
+		"transfer":     "transfer money",
+	}
+
+	notCommands = command{
+		"help": "show this help",
+		"exit": "exit the application",
+	}
+)
+
 func Menu() {
 	var cmd string
 	for true {
 		fmt.Print("Enter command: ")
 		fmt.Scanln(&cmd)
-		switch cmd {
-		case "help":
-			help()
-		case
-			"create", "deposit",
-			"withdraw", "balance",
-			"transactions", "transfer":
-			fetch_responce(cmd)
-		case "exit":
-			return
-		default:
-			fmt.Println("Invalid command")
+		_, ok := commands[cmd]
+		if cmd != "exit" && !ok {
+			fmt.Println("Invalid command!\nAvailable commands:")
+			for command, description := range notCommands {
+				fmt.Printf("%-12s - %s\n", command, description)
+			}
+			for command, description := range commands {
+				fmt.Printf("%-12s - %s\n", command, description)
+			}
+		} else {
+			if ok {
+				fetch_responce(cmd)
+			} else {
+				return
+			}
 		}
 	}
 }
