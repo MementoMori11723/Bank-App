dfde1d357df8a7803b01f69cbd201dab3acebb9e
yasasvi <yasasvigumma@gmail.com>
2025-01-05 13:09:19 +0530

    Made changes
    ...

--- database/middleware/responce.go
+++ database/middleware/responce.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"bank-app/database/bank"
 	"encoding/json"
 	"log/slog"
 	"net/http"
@@ -10,25 +11,25 @@ type ErrorResponse struct {
 	Error string `json:"Error"`
 }
 
-func Responce[T any](next func(*http.Request) (T, error)) http.HandlerFunc {
+func Responce(next func(*http.Request) (bank.Responce, error)) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		slog.Info("Request", r.Method, r.URL.Path)
+		slog.Info("Server - Request", r.Method, r.URL.Path)
+
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
+
 		data, err := next(r)
 		if err != nil {
 			slog.Error(err.Error())
 		}
+
 		err = json.NewEncoder(w).Encode(data)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
-			err := json.NewEncoder(w).Encode(
-				ErrorResponse{
-					Error: err.Error(),
-				},
-			)
+
+			err := json.NewEncoder(w).Encode(ErrorResponse{Error: err.Error()})
 			if err != nil {
-				slog.Error(err.Error())
+				slog.Error("Server - Error", "error", err.Error())
 			}
 		}
 	}
